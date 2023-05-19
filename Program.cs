182b844b21029a7beeaf1cf9697f1a60ab90240d
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2023-05-19 21:23:05 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Лямбда - выражения представляют упрощенную запись анонимных методов. Лямбда-выражения позволяют созда
+//емкие лаконичные методы, которые могут возвращать некоторое значение и которые можно передать в качестве 
+//параметров в другие методы.
+
+//Ламбда-выражения имеют следующий синтаксис: слева от лямбда-оператора => определяется список параметров, 
+//а справа блок выражений, использующий эти параметры:
+
+namespace Lyambda_functions
+{
+
+    delegate void myDelagate(object obj);
+    internal class Program
+    {
+
+        static void test(object obj)
+        {
+            Console.WriteLine("--- {0}",obj);
+        }
+        static void Main(string[] args)
+        {
+            int[] _arr = new int[2] { 1, 2 };
+            int[] _arr01 = { 1, 2, 6, 7 };
+
+            
+            myDelagate Print = (object obj) => {//лямбду функцию складываем в делегат
+                Console.WriteLine(obj);
+            };
+            Print(23);
+            Print += (object i) => {
+            if(int.Parse(i.ToString()) % 2 == 0)
+                {
+                    Console.WriteLine($"{i} - четное");    
+                }
+                    };
+
+            Print += test;
+            Print(23); //вызвали делегат
+            Print(23);
+            foreach (var item in Print.GetInvocationList())
+            {
+                Console.WriteLine(item.Method);
+            }
+            
+            
+
+            
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lyambda_functions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lyambda_functions")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ece2b23b-fb8f-4d29-852f-afff5bb0c253")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
